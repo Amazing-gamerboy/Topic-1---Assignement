73dcc4a9d2a6e6cb6271c23fe8e9c0aaaa67aabe
339720229 <339720229@2260I-L29-005>
2024-09-06 09:39:33 -0400

    done!

--- Topic 1 - Assignement/Program.cs
+++ Topic 1 - Assignement/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Mr Aldworth!"); //Ewan
 
+Console.WriteLine("Comments are used to remind yourself about what code does or where you left off it is also usfull to your peers and colaborators");
+
 Console.WriteLine("EEEEE  RRRR");
 Console.WriteLine("E      R    R");
 Console.WriteLine("E      R    R");
