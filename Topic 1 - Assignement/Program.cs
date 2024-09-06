e77072a0e3cba4f98e418efe8ef4a19c57702b41
339720229 <339720229@2260I-L29-005>
2024-09-06 09:36:12 -0400

    finished the assignment

--- Topic 1 - Assignement/Program.cs
+++ Topic 1 - Assignement/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello Mr Aldworth!"); //Ewan
+
+Console.WriteLine("EEEEE  RRRR");
+Console.WriteLine("E      R    R");
+Console.WriteLine("E      R    R");
+Console.WriteLine("EEEE   RRRR");
+Console.WriteLine("E      R R");
+Console.WriteLine("E      R   R");
+Console.WriteLine("EEEEE  R     R");
+//these are my initials 
